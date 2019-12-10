9ffcd99f9b06dc8b97f26770d4d349596c9d460d
caolk <caolk@mail.jj.cn>
2019-12-10 14:57:33 +0800

    332

--- scripts/usercmd.lua
+++ scripts/usercmd.lua
@@ -1,15 +1,26 @@
-GLOBAL.AddModUserCommand("u-rollback", "rollback", {
-	prettyname = function(command) return "rollback" end,
-	desc = function() return "User Rollback" end,
-	permission = "USER",
-	params = {},
-	emote = false,
-	slash = true,
-	usermenu = false,
-	servermenu = false,
-	vote = false,
-	serverfn = function(params, caller)
-		print("u-rollback by user "..caller.userid)
-		GLOBAL.TheNet:SendWorldRollbackRequestToServer(2)
+-- GLOBAL.AddModUserCommand("u-rollback", "rollback", {
+-- 	prettyname = function(command) return "rollback" end,
+-- 	desc = function() return "User Rollback" end,
+-- 	permission = "USER",
+-- 	params = {},
+-- 	emote = false,
+-- 	slash = true,
+-- 	usermenu = false,
+-- 	servermenu = false,
+-- 	vote = false,
+-- 	serverfn = function(params, caller)
+-- 		print("u-rollback by user "..caller.userid)
+-- 		GLOBAL.TheNet:SendWorldRollbackRequestToServer(2)
+-- 	end
+-- })
+local tonumber = GLOBAL.tonumber
+local _Networking_Say = GLOBAL.Networking_Say
+GLOBAL.Networking_Say = Networking_Say(guid, userid, name, prefab, message, colour, whisper, isemote, ...)
+	_Networking_Say(guid, userid, name, prefab, message, colour, whisper, isemote, ...)
+	if not whisper and string.sub(message, 1, 9) == "/rollback" and string.len(message) == 10 then 
+		local days = tonumber(string.sub(message, 10, 10))
+		if days == 1 or days == 2 then
+			GLOBAL.TheNet:SendWorldRollbackRequestToServer(days)
+		end
 	end
-})
+end
