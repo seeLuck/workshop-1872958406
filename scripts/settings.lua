ff525ad9869cd218d4756b84a34628720b8e1ff4
caolk <caolk@mail.jj.cn>
2019-12-10 16:34:03 +0800

    222

--- scripts/settings.lua
+++ scripts/settings.lua
@@ -39,8 +39,8 @@ if GetModConfigData("smartIceMachine") then
 	GLOBAL.TUNING.EMERGENCY_BURNT_NUMBER = 1
 	GLOBAL.TUNING.EMERGENCY_BURNING_NUMBER = 1 -- number of fires to maintain warning level one automatically
 	GLOBAL.TUNING.EMERGENCY_WARNING_TIME = 1 -- minimum length of warning period
-	GLOBAL.TUNING.EMERGENCY_RESPONSE_TIME = 2 -- BURNT_NUMBER structures must burn within this time period to trigger flingomatic emergency response
-	GLOBAL.TUNING.EMERGENCY_SHUT_OFF_TIME = 10 -- stay on for this length of time
+	GLOBAL.TUNING.EMERGENCY_RESPONSE_TIME = 1 -- BURNT_NUMBER structures must burn within this time period to trigger flingomatic emergency response
+	GLOBAL.TUNING.EMERGENCY_SHUT_OFF_TIME = 20 -- stay on for this length of time
 	GLOBAL.TUNING.FIRESUPPRESSOR_MAX_FUEL_TIME = 480 * 50
 end
 
