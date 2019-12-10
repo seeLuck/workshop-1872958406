c2bde21ad2ac48615e36f767ad73cb23bdfc536d
caolk <caolk@mail.jj.cn>
2019-12-10 15:09:58 +0800

    22

--- scripts/usercmd.lua
+++ scripts/usercmd.lua
@@ -15,9 +15,9 @@
 -- })
 local tonumber = GLOBAL.tonumber
 local _Networking_Say = GLOBAL.Networking_Say
-GLOBAL.Networking_Say = Networking_Say(guid, userid, name, prefab, message, colour, whisper, isemote, ...)
+GLOBAL.Networking_Say = function(guid, userid, name, prefab, message, colour, whisper, isemote, ...)
 	_Networking_Say(guid, userid, name, prefab, message, colour, whisper, isemote, ...)
-	if not whisper and string.sub(message, 1, 9) == "/rollback" and string.len(message) == 10 then 
+	if not whisper and string.sub(message, 1, 9) == "+rollback" and string.len(message) == 10 then 
 		local days = tonumber(string.sub(message, 10, 10))
 		if days == 1 or days == 2 then
 			GLOBAL.TheNet:SendWorldRollbackRequestToServer(days)
