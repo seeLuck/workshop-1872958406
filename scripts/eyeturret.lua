6e2b561b3ea45e12b61d4dd7ef8f690107f9781d
caolk <caolk@mail.jj.cn>
2019-12-16 16:49:46 +0800

    2

--- scripts/eyeturret.lua
+++ scripts/eyeturret.lua
@@ -8,7 +8,9 @@ AddPrefabPostInit("eyeturret", function(inst)
 	inst.components.machine.turnonfn = turnon
 	
 	if inst and inst.components and inst.components.lootdropper then
-		inst.components.lootdropper:AddRandomLoot("eyeturret_item", 1)
-		inst.components.lootdropper.numrandomloot = 1
+		inst.components.lootdropper:AddRandomLoot("deerclops_eyeball", 1)
+		inst.components.lootdropper:AddRandomLoot("thulecite", 4)
+		inst.components.lootdropper:AddRandomLoot("minotaurhorn", 1)
+		inst.components.lootdropper.numrandomloot = 6
 	end
 end)
