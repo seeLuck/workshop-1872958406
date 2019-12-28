30282b0f760e6e393ac5b08cc3a4cbb59b338769
caolk <caolk@mail.jj.cn>
2019-12-28 10:16:51 +0800

    2

---
+++ scripts/seasonstart.lua
@@ -0,0 +1,52 @@
+local OnPlayerSpawn = function(src, player)
+    player.prev_OnNewSpawn = player.OnNewSpawn
+    player.OnNewSpawn = function()
+   
+        local start_items = {}
+        
+        --for season starting items
+        if GLOBAL.TheWorld.state.isspring or (GLOBAL.TheWorld.state.iswinter and GLOBAL.TheWorld.state.remainingdaysinseason < 3) then
+            table.insert(start_items, "umbrella")
+        end
+        --for summer
+        if GLOBAL.TheWorld.state.issummer or (GLOBAL.TheWorld.state.isspring and GLOBAL.TheWorld.state.remainingdaysinseason < 3) then
+            table.insert(start_items, "heatrock")
+            table.insert(start_items, "watermelonhat")
+            for i = 1, 2 do table.insert(start_items, "meatballs") end
+        end
+        --for winter
+        if GLOBAL.TheWorld.state.iswinter or (GLOBAL.TheWorld.state.isautumn and GLOBAL.TheWorld.state.remainingdaysinseason < 3) then
+            table.insert(start_items, "heatrock")
+            table.insert(start_items, "earmuffshat")
+            for i = 1, 2 do table.insert(start_items, "meatballs") end
+        end
+
+        --for all season
+        for i = 1, 4 do table.insert(start_items, "goldnugget") end
+        for i = 1, 10 do table.insert(start_items, "flint") end
+        for i = 1, 6 do table.insert(start_items, "rocks") end
+        for i = 1, 4 do table.insert(start_items, "log") end
+        for i = 1, 6 do table.insert(start_items, "cutgrass") end
+        for i = 1, 6 do table.insert(start_items, "twigs") end
+
+        for k,v in pairs(start_items) do
+            local item = GLOBAL.SpawnPrefab(v)
+            
+            player.components.inventory:GiveItem(item)
+        end   
+         
+        if player.prev_OnNewSpawn ~= nil then
+            player:prev_OnNewSpawn()
+            player.prev_OnNewSpawn = nil
+        end
+    end
+end
+
+
+local function ListenForPlayers(inst)
+    if GLOBAL.TheWorld.ismastersim then
+        inst:ListenForEvent("ms_playerspawn", OnPlayerSpawn)
+    end
+end
+
+AddPrefabPostInit("world", ListenForPlayers)
