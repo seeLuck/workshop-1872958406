166c1d2bf7740a87cb17755e7ac8c30ecea3dc50
caolk <caolk@mail.jj.cn>
2020-08-07 18:10:51 +0800

    1

--- scripts/settings.lua
+++ scripts/settings.lua
@@ -75,34 +75,3 @@ if GetModConfigData("bossFastRespawn") then
 	TUNING.ANTLION_RAGE_TIME_INITIAL = 999 * 480
 	TUNING.ANTLION_RAGE_TIME_MAX = 999 * 480
 end
-
-if GetModConfigData("buildingCloser") then
-	for _,v in pairs(GLOBAL.AllRecipes) do
-		v.min_spacing = 1
-	end
-	AddPrefabPostInit("dug_berrybush", function(inst)
-		if inst.components.deployable then
-			inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
-		end
-	end)
-	AddPrefabPostInit("dug_berrybush2", function(inst)
-		if inst.components.deployable then
-			inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
-		end
-	end)
-	AddPrefabPostInit("dug_berrybush_juicy", function(inst)
-		if inst.components.deployable then
-			inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
-		end
-	end)
-	AddPrefabPostInit("rock_avocado_fruit_sprout", function(inst)
-		if inst.components.deployable then
-			inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
-		end
-	end)
-	AddPrefabPostInit("dug_rock_avocado_bush", function(inst)
-		if inst.components.deployable then
-			inst.components.deployable:SetDeploySpacing(GLOBAL.DEPLOYSPACING.MEDIUM)
-		end
-	end)
-end
