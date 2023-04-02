be58f0f2ead156fdc826b4b804a9cb818f623269
seeluck <810201584@qq.com>
2023-04-02 17:08:06 +0800

    1

--- modinfo.lua
+++ modinfo.lua
@@ -15,6 +15,108 @@ all_clients_require_mod = false
 
 configuration_options =
 {
+	{
+		name = "",
+		label = "休闲配置",
+		hover = "",
+		options =	
+		{
+			{description = "", data = 0},
+		},
+		default = 0,
+	},
+    {
+        name = "boatHealth",
+        label = "BoatHealth",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "ON", data = true, hover = "ON"},
+        },
+        default = false,
+    },
+    {
+        name = "moonStormHealth",
+        label = "MoonStormHealth",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "ON", data = true, hover = "ON"},
+        },
+        default = false,
+    },
+    {
+        name = "seasonStart",
+        label = "SeasonStart",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "Normal", data = 1, hover = "Normal"},
+            {description = "Wealthy", data = 2, hover = "Wealthy"},
+        },
+        default = false,
+    },
+    {
+        name = "fridgeFresh",
+        label = "FridgeFresh",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "ON", data = true, hover = "ON"},
+        },
+        default = false,
+    },
+    {
+        name = "chef",
+        label = "Chef",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "ON", data = true, hover = "ON"},
+        },
+        default = false,
+    },
+    {
+		name = "",
+		label = "体验配置",
+		hover = "",
+		options =	
+		{
+			{description = "", data = 0},
+		},
+		default = 0,
+	},
+    {
+        name = "natureSpawn",
+        label = "NatureSpawn",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "Less", data = 1, hover = "Less"},
+            {description = "More", data = 2, hover = "More"},
+        },
+        default = false,
+    },
+    {
+        name = "worldClean",
+        label = "WorldClean",
+        options =
+        {
+            {description = "OFF", data = false, hover = "OFF"},
+            {description = "ON", data = true, hover = "ON"},
+        },
+        default = false,
+    },
+    {
+		name = "",
+		label = "推荐配置",
+		hover = "",
+		options =	
+		{
+			{description = "", data = 0},
+		},
+		default = 0,
+	},
     {
         name = "usercmd",
         label = "UserCMD",
@@ -56,70 +158,8 @@ configuration_options =
         default = false,
     },
     {
-        name = "boatHealth",
-        label = "BoatHealth",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "ON", data = true, hover = "ON"},
-        },
-        default = false,
-    },
-    {
-        name = "moonStormHealth",
-        label = "MoonStormHealth",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "ON", data = true, hover = "ON"},
-        },
-        default = false,
-    },
-    {
-        name = "natureSpawn",
-        label = "NatureSpawn",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "Less", data = 1, hover = "Less"},
-            {description = "More", data = 2, hover = "More"},
-        },
-        default = false,
-    },
-    {
         name = "lanternColor",
         label = "LanternColor",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "ON", data = true, hover = "ON"},
-        },
-        default = false,
-    },
-    {
-        name = "seasonStart",
-        label = "SeasonStart",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "Normal", data = 1, hover = "Normal"},
-            {description = "Wealthy", data = 2, hover = "Wealthy"},
-        },
-        default = false,
-    },
-    {
-        name = "worldClean",
-        label = "WorldClean",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "ON", data = true, hover = "ON"},
-        },
-        default = false,
-    },
-    {
-        name = "fridgeFresh",
-        label = "FridgeFresh",
         options =
         {
             {description = "OFF", data = false, hover = "OFF"},
@@ -150,16 +190,6 @@ configuration_options =
     {
         name = "dontdrop",
         label = "Dontdrop",
-        options =
-        {
-            {description = "OFF", data = false, hover = "OFF"},
-            {description = "ON", data = true, hover = "ON"},
-        },
-        default = false,
-    },
-    {
-        name = "chef",
-        label = "Chef",
         options =
         {
             {description = "OFF", data = false, hover = "OFF"},
